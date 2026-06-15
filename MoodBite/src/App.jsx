8d696ace14675a07f1ff0e65494a433b54a45075
ayush shrestha <ayyush227@gmail.com>
2026-06-15 08:05:31 +0545

    react

--- MoodBite/src/App.jsx
+++ MoodBite/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -7,16 +7,35 @@ import Header from './components/Header'
 import ApiSetup from './components/ApiSetup'
 
 function App() {
-const [showApiKey,setShowApiKey]=useState(true)
+  const [showApiKey, setShowApiKey] = useState(false)
+  const [apiKeyInput, setApiKeyInput] = useState("")
+  //to save in localstorage
+  const [apiKey,setApiKey]=useState("")
 
-if(showApiKey){
-  return(
-    <ApiSetup/>
-  )
-}
+  const handleApiKeySubmit = (e) => { // e helps to trigger an event
+    e.preventDefault() // stops the webpage to reload
+    if (apiKeyInput) { // if user types anything in the input box then its stored in apiKeyInput
+      setApiKey(apiKeyInput) // store the apiKeyInput in setApiKey
+      setShowApiKey(false) // hide the component ApiSetup
+    }
+  }
+
+  useEffect(()=>{
+    if(apiKey){
+      localStorage.setItem("apiKey",apiKey)
+    }
+  },[apiKey])
+
+  { console.log(apiKeyInput) }
+
+  if (showApiKey) {
+    return (
+      <ApiSetup onSubmit={handleApiKeySubmit} setApiKeyInput={setApiKeyInput} apiKeyInput={apiKeyInput} />
+    )
+  }
   return (
     <>
-    <Header/>
+      <Header onChangeApiKey={() => setShowApiKey(true)} />
     </>
   )
 }
--- MoodBite/src/components/ApiSetup.jsx
+++ MoodBite/src/components/ApiSetup.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 
-const ApiSetup = () => {
-    return (
-        <div className='bg-neutral-600'>
-            <div className='max-w-6xl mx-auto '>
-                <h1>Mood Bite</h1>
-                <p>AI Powered recipe for every feelings</p>
+// camelCase = javascript
+// snake_case = python
 
-                <div>
+const ApiSetup = ({apiKeyInput,setApiKeyInput,onSubmit}) => {
+    return (
+        <div className='bg-neutral-600 flex items-center justify-center google h-screen'>
+            <div className='max-w-6xl mx-auto text-white'>
+                <h1 className='text-center text-3xl font-bold google'>Mood Bite</h1>
+                <p className='text-center text-lg my-3'>AI Powered recipe for every feelings</p>
+
+                <div className='text-center'>
                     <p>Get your free key at</p>
                     <a href='https://aistudio.google.com/app/apikey' target='_blank' >aistudio.google.com</a>
                 </div>
+
+                <form onSubmit={onSubmit}>
+                    <input 
+                    value={apiKeyInput}
+                    onChange={(e)=>setApiKeyInput(e.target.value)}
+                    type='text' placeholder='Enter your api key' className='bg-white m-4 text-black p-2 w-full rounded-full'></input>
+                    <button type='submit' className='bg-white rounded-full px-4 py-2 text-black border border-blue-400'>Start Cooking</button>
+                </form>
             </div>
         </div>
     )
